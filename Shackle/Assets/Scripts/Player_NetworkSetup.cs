0009f57cf7d9241500662e8ace63b9cf70aace54
ConnorsRM <ryanmconnors@gmail.com>
2016-01-13 09:25:29 -0800

    Networking Implementation Phase 1
    + Network Manager Functional Local
    + Main Menu provides Debug UI for connection
    + Object Spawns on connection
    -  Object Spawn is NOT at set position relative to player-host yet.

---
+++ Shackle/Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+
+    [SerializeField]
+    Camera FPSCharacterCam;
+    [SerializeField]
+    AudioListener audiolistener;
+    // Use this for initialization
+    void Start () {
+	
+        if(isLocalPlayer)
+        {
+            GameObject.Find("Main Camera").SetActive(false);
+            GetComponent<CharacterController>().enabled = true;
+            GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+            FPSCharacterCam.enabled = true;
+            audiolistener.enabled = true;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
