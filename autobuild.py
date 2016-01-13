98f57ebae2346603fd54eeab0ee276c68eb9fbe3
ConnorsRM <ryanmconnors@gmail.com>
2016-01-13 10:45:02 -0800

    autobuild Update
    changed for unix case sensitivity.

--- autobuild.py
+++ autobuild.py
@@ -2,9 +2,9 @@ import subprocess
 
 #Check for env variable and terminate if not found
 
-subprocess.call("Echo Beginning Build Process. Initiating Git Pull... | slacker -c buildnotes", shell=True)
+subprocess.call("echo Beginning Build Process. Initiating Git Pull... | slacker -c buildnotes", shell=True)
 subprocess.call("git pull", shell=True)
-subprocess.call("Echo Git Pull Complete. | slacker -c buildnotes", shell=True)
-subprocess.call("Echo Launching Virtual Machine | slacker -c buildnotes", shell=True)
+subprocess.call("echo Git Pull Complete. | slacker -c buildnotes", shell=True)
+subprocess.call("echo Launching Virtual Machine | slacker -c buildnotes", shell=True)
 # Open VM and Performing Build with seperate script on startup
-subprocess.call("Virtual Machine Closed. Build Complete. | slacker -c buildnotes", shell=True)
+subprocess.call("echo Virtual Machine Closed. Build Complete. | slacker -c buildnotes", shell=True)
