40f09264b283a11db00d4eb0ceb986e7e60d784e
Hayden Platt <hplatt@adgpm.soe.ucsc.edu>
2016-02-01 11:40:04 -0800

    Flashlight first pass, and LightTest scene

---
+++ Shackle/Assets/Scripts/spt_flashlight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spt_flashlight : MonoBehaviour {
+
+    public Light flashlight;
+
+    // Use this for initialization
+    void Awake()
+    {
+        flashlight = gameObject.GetComponent<Light>();
+        flashlight.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (spt_playerControls.rightThumbstickButtonPressed() || Input.GetKeyDown(KeyCode.F))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
+    }
+}
