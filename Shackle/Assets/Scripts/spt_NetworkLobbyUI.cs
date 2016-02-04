9db2bdfed961e2b9ac3c0fe9eca5363a0725dfbb
ConnorsRM <ryanmconnors@gmail.com>
2016-02-04 13:41:18 -0800

    Network Connections Interface in VR

---
+++ Shackle/Assets/Scripts/spt_NetworkLobbyUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class spt_NetworkLobbyUI : MonoBehaviour {
+
+    public NetworkManager manager;
+
+    void Awake() {
+        manager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+    }
+
+    //clearly this shouldn't be used outside of development.
+    public void connectLocal() {
+        manager.networkAddress = "localhost";
+        manager.StartClient();
+    }
+
+    public void hostGame() {
+        manager.StartHost();
+    }
+}
