be27b0220c4742f617680231283bd6519e5f7cc3
Moofkin <cclauren15@gmail.com>
2016-02-05 16:44:15 -0800

    Beginning Work on Item Interaction
    This push contains one new piece of code that will eventually completely
    control the monster's item interaction capabilities. The current version
    is just the monster's behaviors, and does not contain the network puzzle
    logic integration.

---
+++ Shackle/Assets/Scripts/spt_monster_interaction.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spt_monster_interaction : MonoBehaviour {
+
+    private spt_NetworkPuzzleLogic network;
+
+    public Transform[] interactableObjects;
+    private double[] weights;
+
+    private int interactionDowntime = 30;
+    private int currentTime = 0;
+    private int lastInteractionTime = 0;
+
+	// Use this for initialization
+	void Start () {
+        network = GameObject.FindObjectOfType(typeof(spt_NetworkPuzzleLogic)) as spt_NetworkPuzzleLogic;
+        InvokeRepeating("updateTime", 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((currentTime - lastInteractionTime) > interactionDowntime){
+            for (int i = 0; i < interactableObjects.Length; i++){
+                if (Vector3.Distance(interactableObjects[i].position, gameObject.transform.position) < 2){
+
+                }
+            }
+        }
+	}
+
+    void interactWithObject(string item){
+        network.updatePuzzleState(item, false);
+    }
+
+    void updateTime(){
+        currentTime = currentTime + 1;
+    }
+}
