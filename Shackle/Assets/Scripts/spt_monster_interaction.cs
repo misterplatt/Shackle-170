425e061899b4d7ca1324b665f4d1927c4d8f5634
Moofkin <cclauren15@gmail.com>
2016-02-06 18:36:48 -0800

    Item Interaction
    It's all hooked up to the network manager and (hopefully) ready to go.
    Still need to perform tests on game instances with the network enabled.

--- Shackle/Assets/Scripts/spt_monster_interaction.cs
+++ Shackle/Assets/Scripts/spt_monster_interaction.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class spt_monster_interaction : MonoBehaviour {
 
     private spt_NetworkPuzzleLogic network;
 
-    public Transform[] interactableObjects;
+    private GameObject[] interactableObjects;
+    private string[] interactableObjectNames;
     private double[] weights;
 
     private int interactionDowntime = 30;
@@ -15,22 +17,32 @@ public class spt_monster_interaction : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         network = GameObject.FindObjectOfType(typeof(spt_NetworkPuzzleLogic)) as spt_NetworkPuzzleLogic;
-        InvokeRepeating("updateTime", 1, 1);
+        List<dev_LogicPair>.Enumerator e = network.devtool_PuzzleStates.GetEnumerator();
+        int index = 0;
+        while (e.MoveNext()){
+            interactableObjects[index] = e.Current.item;
+            interactableObjectNames[index] = e.Current.eventName;
+            weights[index] = 0.5;
+            index++;
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if ((currentTime - lastInteractionTime) > interactionDowntime){
             for (int i = 0; i < interactableObjects.Length; i++){
-                if (Vector3.Distance(interactableObjects[i].position, gameObject.transform.position) < 2){
-
+                if (Vector3.Distance(interactableObjects[i].transform.position, gameObject.transform.position) < 2){
+                    float decision = Random.Range(0, 1);
+                    if (decision < weights[i]){
+                        interactWithObject(interactableObjectNames[i], interactableObjects[i].name);
+                    }
                 }
             }
         }
 	}
 
-    void interactWithObject(string item){
-        network.updatePuzzleState(item, false);
+    void interactWithObject(string item, string itemName){
+        network.updatePuzzleState(item, false, itemName);
     }
 
     void updateTime(){
