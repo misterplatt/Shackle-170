2ddf9ef4fa1bbaf34093b562492e84db4a428c25
unknown <rmconnor@cmdrkeen.adgpm.soe.ucsc.edu>
2016-02-12 11:29:04 -0800

    Nuclear Launch Detected

--- Shackle/Assets/Scripts/Network/spt_WorldState.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
-using System.Collections;
-
-public class spt_WorldState : NetworkBehaviour {
-    public static bool worldStateChanged = false;
-}
