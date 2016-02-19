0e0d3dac77c8860d331b1ac99f2d392f62aab8d0
Hayden Platt <misterplatt17@gmail.com>
2016-02-19 11:23:57 -0800

    Added display/remove timing functionality to toolTipListener

--- Shackle/Assets/Scripts/spt_toolTipListener.cs
+++ Shackle/Assets/Scripts/spt_toolTipListener.cs
@@ -15,8 +15,12 @@ Listens for event cues to show and hide tooltips.
 public class spt_toolTipListener : MonoBehaviour {
 
     public Sprite empty;
-    public Sprite interaction;
-    public Sprite movable;
+    public Sprite aButton;
+    public Sprite LS_A;
+    public Sprite triggers;
+    public Sprite RS_B;
+    public Sprite xButton;
+    public Sprite RS;
 
     private Image currentImage;
     private Text currentText;
@@ -25,27 +29,31 @@ public class spt_toolTipListener : MonoBehaviour {
 	void Start () {
         currentImage = GetComponentInChildren<Image>();
         currentText = GetComponentInChildren<Text>();
+        StartCoroutine(setToolTip(RS, "Press to Toggle Flashlight", 3f));
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.H)) {
-            clearToolTip();
-        }
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            setToolTip(movable, "To Move Some Objects");
-        }
+        if (Input.GetKeyDown(KeyCode.H)) clearToolTip();
+
+        if (Input.GetKeyDown(KeyCode.J)) StartCoroutine(setToolTip(LS_A, "To Move Some Objects", 1f));
+    }
+
+    //Function which sets the toolTip image and text after delayTime seconds, then clears after 3 seconds
+    IEnumerator setToolTip(Sprite newSprite, string newText, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        // Now do your thing here
+        currentImage.sprite = newSprite;
+        currentText.text = newText;
+        Invoke("clearToolTip", 3f);
     }
 
     //Empties the tool tip display
-    public void clearToolTip() {
+    public void clearToolTip()
+    {
         currentImage.sprite = empty;
         currentText.text = "";
     }
 
-    public void setToolTip(Sprite newSprite, string newText) {
-        currentImage.sprite = newSprite;
-        currentText.text = newText;
-    }
 }
