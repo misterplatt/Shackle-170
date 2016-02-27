b1743efeaa6b25cd8c926ec79bc807d14f9d5c00
unknown <rmconnor@link.adgpm.soe.ucsc.edu>
2016-02-26 16:15:54 -0800

    Sound Debug Tool

---
+++ Shackle/Assets/Scripts/Debug Tools/spt_debug_soundTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spt_debug_soundTest : MonoBehaviour {
+    // Make sure to attatch the audio souce to this script in the Unity editor!
+    public AudioSource source;
+
+    // Array of warning noises. This array needs to be instantiated in the Unity editor.
+    public AudioClip sound;
+
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (source == null)
+        {
+            source = GameObject.Find("MonsterStandin").GetComponent<AudioSource>();
+            source.clip = sound;
+        }
+	    if (Input.GetKeyDown(KeyCode.P)) source.PlayOneShot(source.clip);
+    }
+}
