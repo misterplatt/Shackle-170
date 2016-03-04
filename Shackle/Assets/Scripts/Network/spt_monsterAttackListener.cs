41c37c5c40f36e9e544607a052fdfdd3405948d9
Ryan Connors <rmconnor@adgpm.soe.ucsc.edu>
2016-03-04 14:08:14 -0800

    Network Attack Second Pass

--- Shackle/Assets/Scripts/Network/spt_monsterAttackListener.cs
+++ Shackle/Assets/Scripts/Network/spt_monsterAttackListener.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class spt_monsterAttackListener : MonoBehaviour {
-
+public class spt_monsterAttackListener : NetworkBehaviour {
+    public bool atkMsgSent = false;
 	// Use this for initialization
 	void Start () {
-	
+	    
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (isServer) return;
+
+        GameObject monster = GameObject.FindGameObjectWithTag("monster");
+        spt_monsterMotivation moto = monster.GetComponent<spt_monsterMotivation>();
+
+        if (moto.isAttacking) Cmd_ClientSensedAttack();
 	}
+
+    //Command tells monster that client has recieved packet update and is ready to actually attack.
+    [Command]
+    public void Cmd_ClientSensedAttack() {
+
+        GameObject monster = GameObject.FindGameObjectWithTag("monster");
+        spt_monsterMotivation moto = monster.GetComponent<spt_monsterMotivation>();
+        moto.clientRecievedSignal = true;
+    }
+
 }
