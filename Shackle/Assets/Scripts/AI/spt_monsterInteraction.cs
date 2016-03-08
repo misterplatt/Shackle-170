926e2926738d77abe9e45b32eabb4e70853a5ed1
Moofkin <cclauren15@gmail.com>
2016-03-07 19:22:45 -0800

    AI Interaction Adjustments
    Made adjustments to the monster, as per today's suggestions. The
    downtime between interactions was doubled. In addition, the monster
    **should** only try to interact with objects whose puzzle state has been
    flipped to true. (These need to be tested, hard to do so without
    headsets)

--- Shackle/Assets/Scripts/AI/spt_monsterInteraction.cs
+++ Shackle/Assets/Scripts/AI/spt_monsterInteraction.cs
@@ -2,7 +2,7 @@
  * 
  * Created by: Lauren Cunningham
  * 
- * Last Revision Date: 3/5/2016
+ * Last Revision Date: 3/7/2016
  * 
  * This file is the one that governs all interactions between the monster and items in the environment. **/
 
@@ -25,7 +25,7 @@ public class spt_monsterInteraction : MonoBehaviour {
     // Downtime = the amount of time the monster must wait until it can perform another interaction i.e. the time between interactions.
     // Current Time = the amount of time elapsed in this playthrough, in seconds.
     // Last Interaction Time = the elapsed time of the last interaction
-    private int interactionDowntime = 30;
+    private int interactionDowntime = 60;
     private int currentTime = 0;
     private int lastInteractionTime = 0;
 
@@ -84,7 +84,7 @@ public class spt_monsterInteraction : MonoBehaviour {
                     {
 
                         // If the monster is within interaction range...
-                        if (Vector3.Distance(GameObject.Find(network.PuzzleStates[indecies[i]].itemName).transform.position, gameObject.transform.position) < 2)
+                        if (Vector3.Distance(GameObject.Find(network.PuzzleStates[indecies[i]].itemName).transform.position, gameObject.transform.position) < 2 && checkIfInteractableYet(network.PuzzleStates[indecies[i]].itemName))
                         {
 
                             // Perform interaction some of the time, dependent on a random number.
@@ -104,6 +104,18 @@ public class spt_monsterInteraction : MonoBehaviour {
         }
 	}
 
+    // This function checks if an interactable puzzle state is true or false. Used to determine if the monster can perform an action on an interactable object.
+    public bool checkIfInteractableYet(string itemName){
+        for (int index = 0; index < network.PuzzleStates.Count; ++index)
+        {
+            if (network.PuzzleStates[index].itemName == gameObject.name && network.PuzzleStates[index].state == true)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    
     // Function used to make updates to the network puzzle state communicator.
     public void interactWithObject(string item, string itemName){
         Debug.Log("interacting with: " + itemName);
