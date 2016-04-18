2bf9ea31081abe603e9ed38304ce429f11aec9d5
Hayden Platt <hplatt@adgpm.soe.ucsc.edu>
2016-04-18 12:14:44 -0700

    Added victory light to Ranger, and "animated" its entrance to the room

--- Shackle/Assets/Scripts/Player-side/spt_victoryListener.cs
+++ Shackle/Assets/Scripts/Player-side/spt_victoryListener.cs
@@ -3,7 +3,7 @@ spt_victoryListener
 
 Author(s): Hayden Platt
 
-Revision 1
+Revision 2
 
 Listens for NPL puzzle completion to show the 
 "YOU WIN" UI Text.
@@ -16,6 +16,14 @@ using System.Collections;
 public class spt_victoryListener : MonoBehaviour
 {
     private bool once = false;
+    private bool expandLight = false;
+
+    private Light winLight;
+
+    void Start()
+    {
+        winLight = GameObject.Find("victory_light").GetComponent<Light>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -24,10 +32,14 @@ public class spt_victoryListener : MonoBehaviour
         if (GameObject.FindWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>().PuzzleStates[0].state == true && !once)
         {
             GetComponent<Text>().enabled = true;
-            GameObject.Find("victory_light").GetComponent<Light>().enabled = true;
+            expandLight = true;
             //GameObject.Find("player_B_light").GetComponent<Light>().enabled = true;
             transform.parent.FindChild("FadePanel").GetComponent<VRStandardAssets.Utils.VRCameraFade>().FadeOut(false);
             once = true;
+        }
+
+        if (expandLight && winLight.spotAngle < 179) {
+            winLight.spotAngle += 0.1f;
         }
     }
 }
