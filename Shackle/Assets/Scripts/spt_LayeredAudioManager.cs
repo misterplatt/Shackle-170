589768f030240e8ee597086749bafbfefbb1b344
unknown <ddiba@cmdrkeen.adgpm.soe.ucsc.edu>
2016-04-25 13:38:14 -0700

    Beginnings of Layered Audio System

---
+++ Shackle/Assets/Scripts/spt_LayeredAudioManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spt_LayeredAudioManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Fabric.EventManager.Instance.PostEvent("BackgroundMusic");
+        //Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "SecondLayer", 0, null);
+        //Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "ThirdLayer", 0, null);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("z")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "SecondLayer", 1.0f, null);
+        if (Input.GetKeyDown("x")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "ThirdLayer", 1.0f, null);
+        if (Input.GetKeyDown("c")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FourthLayer", 1.0f, null);
+        if (Input.GetKeyDown("v")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FifthLayer", 1.0f, null);
+
+
+
+    }
+}
