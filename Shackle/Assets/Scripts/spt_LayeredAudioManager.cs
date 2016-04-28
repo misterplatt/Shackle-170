ced3e216a1b840ede994c74d40bdc7cdccebc2a5
unknown <ddiba@seamus.adgpm.soe.ucsc.edu>
2016-04-27 17:40:41 -0700

    Some more ish done to the layered audio. Still need to add the 6th layer dip stuff.

--- Shackle/Assets/Scripts/spt_LayeredAudioManager.cs
+++ Shackle/Assets/Scripts/spt_LayeredAudioManager.cs
@@ -1,16 +1,36 @@
-﻿using UnityEngine;
+﻿/*
+spt_LayeredAudioManager
+
+Author(s): The Dara Diba
+
+Revision 1
+
+This is dynamic/layered audio manager that kicks booty
+*/
+
+using UnityEngine;
 using System.Collections;
 
 public class spt_LayeredAudioManager : MonoBehaviour {
 
 
     private float getSchwifty;
-	// Use this for initialization
-	void Start () {
+
+    public AudioClip fifthLayerAOne;
+    public AudioClip fifthLayerATwo;
+    private Fabric.Component fifthLayerComponents;
+    private float roll;
+    private bool rolled;
+    private bool once;
+    private GameObject fuck;
+
+    // Use this for initialization
+    void Start () {
         Fabric.EventManager.Instance.PostEvent("BackgroundMusic");
+        once = false;
         getSchwifty = 0;
-        //Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "SecondLayer", 0, null);
-        //Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "ThirdLayer", 0, null);
+
+        fifthLayerComponents = Fabric.FabricManager.Instance.GetComponentByName("Audio Component");
 
     }
 	
@@ -30,11 +50,46 @@ public class spt_LayeredAudioManager : MonoBehaviour {
         if (getSchwifty >= 60) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FourthLayer", 1.0f, null);
         else Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FourthLayer", 0f, null);
 
-        if (getSchwifty >= 10) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FifthLayer", 1.0f, null);
-        else Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FifthLayer", 0f, null);
-        // if (Input.GetKeyDown("z")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "SecondLayer", 1.0f, null);
-        //if (Input.GetKeyDown("x")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "ThirdLayer", 1.0f, null);
-        //if (Input.GetKeyDown("c")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FourthLayer", 1.0f, null);
-        //if (Input.GetKeyDown("v")) Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FifthLayer", 1.0f, null);
+        if (getSchwifty >= 80)
+        {
+            if (once == false)
+            {
+                RolltheDieBitch("FifthLayer1", "FifthLayer2");
+                once = true;
+            }
+        }
+        else
+        {
+            once = false;
+            Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FifthLayer1", 0f, null);
+            Fabric.EventManager.Instance.SetParameter("BackgroundMusic", "FifthLayer2", 0f, null);
+        }
+        if(getSchwifty >= 100)
+        {
+
+        }
+        else
+        {
+
+        }
+      
+    }
+
+    void RolltheDieBitch(string layerNameOne, string layerNameTwo)
+    {
+        roll = Random.Range(0f, 10f);
+        rolled = false;
+
+        if (roll > 5f && rolled == false)
+        {
+            //Debug.Log("SUCK A FUCK");
+            Fabric.EventManager.Instance.SetParameter("BackgroundMusic", layerNameOne, 1.0f, null);
+            rolled = true;
+        }
+        else
+        {
+            //Debug.Log("DON'T SUCK A FUCK");
+            Fabric.EventManager.Instance.SetParameter("BackgroundMusic", layerNameTwo, 1.0f, null);
+        }
     }
 }
