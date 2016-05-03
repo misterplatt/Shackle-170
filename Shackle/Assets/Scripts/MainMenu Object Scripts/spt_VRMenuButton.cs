47768fcb3f52c0c416196684bc12460aff404eae
unknown <hplatt@link.adgpm.soe.ucsc.edu>
2016-05-03 12:27:40 -0700

    Added UI to lobby manager, fixed button color issue in VRMainMenu

--- Shackle/Assets/Scripts/MainMenu Object Scripts/spt_VRMenuButton.cs
+++ Shackle/Assets/Scripts/MainMenu Object Scripts/spt_VRMenuButton.cs
@@ -27,13 +27,13 @@ namespace VRStandardAssets.Examples
         }
 
 
-        //Show associated text while reticle is over the object
+        //Set button color to red while reticle is over the object
         protected override void HandleOver()
         {
             button.image.color = new Color32(151, 0, 0, 255);
         }
 
-        //Hide associated text while reticle leaves the object
+        //Return button to grey while reticle leaves the object
         protected override void HandleOut()
         {
             button.image.color = new Color32(55, 55, 55, 255);
@@ -43,6 +43,24 @@ namespace VRStandardAssets.Examples
         {
             button.onClick.Invoke(); //Calls whatever is placed in the Button component's OnClick sections
             holding = false;
+        }
+
+        //Code to be called in onClick for level select
+        public void selectLevel() {
+            //Set all level buttons to grey (unselected)
+            foreach (Transform child in transform.parent)
+            {
+                if (child.name.Contains("lvl_")) child.GetComponent<Button>().image.color = new Color32(151, 0, 0, 255);
+            }
+            //Then set the clicked level button to red (selected)
+            GetComponent<Button>().image.color = new Color32(0, 0, 0, 255);
+        }
+
+        //Function to be called in onClick when Ready button is pressed
+        public void readyUp() {
+            //Depending on which ready button was clicked, change the ready state "button" color to green
+            if (transform.parent.name == "Host_UI") transform.parent.FindChild("P1_readystate").GetComponent<Button>().image.color = new Color32(147, 196, 125, 255);
+            else transform.parent.FindChild("P1_readystate").GetComponent<Button>().image.color = new Color32(147, 196, 125, 255);
         }
     }
 }
