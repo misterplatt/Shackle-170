95f3fa362e8c0c979b95eacfadf8c6fcfdebffb6
unknown <hplatt@link.adgpm.soe.ucsc.edu>
2016-05-03 16:53:32 -0700

    Hayden Platt is a dummy

--- Shackle/Assets/Scripts/MainMenu Object Scripts/spt_VRMenuButton.cs
+++ Shackle/Assets/Scripts/MainMenu Object Scripts/spt_VRMenuButton.cs
@@ -44,23 +44,5 @@ namespace VRStandardAssets.Examples
             button.onClick.Invoke(); //Calls whatever is placed in the Button component's OnClick sections
             holding = false;
         }
-
-        //Code to be called in onClick for level select
-        public void selectLevel() {
-            //Set all level buttons to grey (unselected)
-            foreach (Transform child in transform.parent)
-            {
-                if (child.name.Contains("lvl_")) child.GetComponent<Button>().image.color = new Color32(151, 0, 0, 255);
-            }
-            //Then set the clicked level button to red (selected)
-            GetComponent<Button>().image.color = new Color32(0, 0, 0, 255);
-        }
-
-        //Function to be called in onClick when Ready button is pressed
-        public void readyUp() {
-            //Depending on which ready button was clicked, change the ready state "button" color to green
-            if (transform.parent.name == "Host_UI") transform.parent.FindChild("P1_readystate").GetComponent<Button>().image.color = new Color32(147, 196, 125, 255);
-            else transform.parent.FindChild("P1_readystate").GetComponent<Button>().image.color = new Color32(147, 196, 125, 255);
-        }
     }
 }
