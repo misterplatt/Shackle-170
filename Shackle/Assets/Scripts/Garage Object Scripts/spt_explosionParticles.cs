ea2e2afdac2e8a62d5407a1de1cc2adb4c4e8406
Lauren Cunningham <cclauren15@gmail.com>
2016-05-11 22:48:47 -0700

    Work on the Optics Lab
    Didn't manage to get all the particles in, but it should be functional
    for playtests and such.

---
+++ Shackle/Assets/Scripts/Garage Object Scripts/spt_explosionParticles.cs
@@ -0,0 +1,52 @@
+﻿/* spt_explosionParticles.cs
+ * 
+ * Created by: Lauren Cunningham
+ * 
+ * Last Revision Date: 5/11/2016
+ * 
+ * This script the explosion form the TNT levers. **/
+
+using UnityEngine;
+using System.Collections;
+
+public class spt_explosionParticles : MonoBehaviour {
+
+    private int playerATNT = -1;
+    private int playerBTNT = -1;
+    private spt_NetworkPuzzleLogic network;
+
+    private bool once = false;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (network == null)
+        {
+            network = GameObject.FindGameObjectWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>();
+            for (int i = 0; i < network.PuzzleStates.Count; i++)
+            {
+                if (network.PuzzleStates[i].name == "leverAPressed")
+                    playerATNT = i;
+                if (network.PuzzleStates[i].name == "leverBPressed")
+                    playerBTNT = i;
+            }
+        }
+        else
+        {
+            if ((network.PuzzleStates[playerATNT].state == true) && (network.PuzzleStates[playerBTNT].state == true))
+            {
+                if (!once)
+                {
+                    GameObject go = (GameObject)Instantiate(Resources.Load("Explosion"));
+                    go.transform.position = gameObject.transform.position;
+                    once = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ Shackle/Assets/Scripts/Optics Object Scripts/spt_laserMachineParticles.cs
@@ -0,0 +1,45 @@
+﻿/* spt_laserMachineParticles.cs
+ * 
+ * Created by: Lauren Cunningham
+ * 
+ * Last Revision Date: 5/11/2016
+ * 
+ * This script turns on and off the laser machine's particle system as the machine is turned on and off. **/
+
+using UnityEngine;
+using System.Collections;
+
+public class spt_laserMachineParticles : MonoBehaviour {
+
+    private int puzzleStateIndex = -1;
+    private spt_NetworkPuzzleLogic network;
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (network == null)
+        {
+            network = GameObject.FindGameObjectWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>();
+            for (int i = 0; i < network.PuzzleStates.Count; i++)
+            {
+                if (network.PuzzleStates[i].name == "isLaserOn")
+                    puzzleStateIndex = i;
+            }
+        }
+        else
+        {
+            if (network.PuzzleStates[puzzleStateIndex].state == true)
+            {
+                gameObject.GetComponent<ParticleSystem>().enableEmission = true;
+            }
+            else
+                gameObject.GetComponent<ParticleSystem>().enableEmission = false;
+        }
+
+	}
+}
