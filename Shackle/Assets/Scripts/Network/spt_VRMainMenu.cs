0d3a58f5e9cbe0ed1f3fe40b38a32f6d56e47cf2
Ryan Connors <rmconnor@adgpm.soe.ucsc.edu>
2016-05-11 17:49:17 -0700

    Partial Lobby NetworkTransport Killer

---
+++ Shackle/Assets/Scripts/Network/spt_VRMainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class spt_VRMainMenu : NetworkBehaviour {
+
+    NetworkManager manager;
+    NetworkDiscovery discovery;
+
+	// Use this for initialization
+	void Start () {
+        manager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+        discovery = GameObject.Find("NetworkDiscovery").GetComponent<NetworkDiscovery>();    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
