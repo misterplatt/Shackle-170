3b51891f214e10a48a48624403b7a645eb7a43d9
Ryan Connors <rmconnor@adgpm.soe.ucsc.edu>
2016-05-13 10:31:17 -0700

    Partial Command

--- Shackle/Assets/Scripts/Optics Object Scripts/spt_laserSwitch.cs
+++ Shackle/Assets/Scripts/Optics Object Scripts/spt_laserSwitch.cs
@@ -47,6 +47,35 @@ namespace VRStandardAssets.Examples
             if (Input.GetKeyDown(KeyCode.L)) currentState = !currentState;
             //GameObject.FindGameObjectWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>().Cmd_UpdatePuzzleLogic("isLaserOn", true, "Joystick_base")
 
+            currentState = GameObject.FindGameObjectWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>().PuzzleStates[4].state;
+            //Change laser LineRenderer's enabled status on switch click
+            if (currentState == true)
+            {
+                aSource.clip = laserStart;
+                aSource.Play();
+                Invoke("LaserMachinePurr", 10f);
+                laserMesh.enabled = currentState;
+                laserCollider.enabled = currentState;
+                metalSwitch.transform.eulerAngles = new Vector3(-25.5f, 2.3f, -4f);
+
+                //NPL Update
+                local_isLaserOn = true;
+                spt_WorldState.worldStateChanged = true;
+
+            }
+            else if (currentState == false)
+            {
+                aSource.Stop();
+                aSource.loop = false;
+                laserMesh.enabled = currentState;
+                laserCollider.enabled = currentState;
+                metalSwitch.transform.position = initialPosition;
+                metalSwitch.transform.rotation = initialRotation;
+
+                //NPL Update
+                local_isLaserOn = false;
+                spt_WorldState.worldStateChanged = true;
+            }
             //POSSIBLY REMEDIED BY HAVING MONSTER'S RESET ITEM JUST CALL CLICKSUCCESS()?
             /*if (GameObject.FindGameObjectWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>().PuzzleStates[4].state == false)
             {
