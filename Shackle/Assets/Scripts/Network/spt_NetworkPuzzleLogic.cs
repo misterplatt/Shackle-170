b3dae5aa810ccee08b2b1bd6f7529b0fb76d8ee6
Hayden Platt <hplatt@adgpm.soe.ucsc.edu>
2016-05-15 15:35:00 -0700

    Trying to fix laserHitLock shutting off laser

--- Shackle/Assets/Scripts/Network/spt_NetworkPuzzleLogic.cs
+++ Shackle/Assets/Scripts/Network/spt_NetworkPuzzleLogic.cs
@@ -211,14 +211,14 @@ public class spt_NetworkPuzzleLogic : NetworkBehaviour {
                 {
                     Debug.Log("Updating laserHitLock on the network to " + VRStandardAssets.Examples.spt_chestListener.local_laserHitLock);
                     GetComponent<spt_NetworkPuzzleLogic>().Cmd_UpdatePuzzleLogic("laserHitLock", true, "mdl_chestLock");
+                    spt_WorldState.worldStateChanged = false;
+                    return;
                 }
                 //If a laser has intersected with the security panel, update server state
                 if (VRStandardAssets.Examples.spt_panelListener.local_laserHitPanel)
                 {
                     Debug.Log("Updating laserHitPanel on the network to " + VRStandardAssets.Examples.spt_panelListener.local_laserHitPanel);
                     GetComponent<spt_NetworkPuzzleLogic>().Cmd_UpdatePuzzleLogic("laserHitPanel", true, "Security Panel");
-                    spt_WorldState.worldStateChanged = false;
-                    return;
                 }
                 //If the player turns on or off the laser machine, update server state
                 if (VRStandardAssets.Examples.spt_laserSwitch.local_isLaserOn != PuzzleStates[4].state)
