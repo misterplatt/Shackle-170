75bbf594a96433bef73063b80208af6de51f4a92
Lauren Cunningham <cclauren15@gmail.com>
2016-05-16 20:54:38 -0700

    Push With Most of the Bucket Chuck Completed
    Known Issue:
    The reset function isn't called properly on the bucket, most likely due
    to the presence of two baseInteractives on it.

--- Shackle/Assets/Scripts/Garage Object Scripts/spt_bucketChuck.cs
+++ Shackle/Assets/Scripts/Garage Object Scripts/spt_bucketChuck.cs
@@ -3,7 +3,7 @@ spt_bucketChuck
 
 Author(s): Hayden Platt, Dara Diba, Lauren Cunningham
 
-Revision 1
+Revision 2
 
 Script which allows the monster to interact with the bucket, and knock it off
 the shelf. Once complete, it disables the bucket's movable script so that
@@ -19,8 +19,14 @@ namespace VRStandardAssets.Examples
 {
     public class spt_bucketChuck : spt_baseInteractiveObject
     {
-
         Rigidbody rb;
+
+        bool networkInitialized = false;
+        spt_NetworkPuzzleLogic network;
+
+        //Puzzle state indices of the bucket being on the shelf, and the noise of the bucket hitting the floor
+        int bucketFlingIndex = -1;
+        int bucketCollisionIndex = -1;
 
         override protected void Start()
         {
@@ -31,16 +37,49 @@ namespace VRStandardAssets.Examples
         {
             //DEBUG KEY: Simulates bucket chuck
             if (Input.GetKeyDown(KeyCode.X)) resetItem();
+
+            // Gets all the needed network components once only
+            if (!networkInitialized)
+            {
+                network = GameObject.FindWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>();
+                if (network != null)
+                {
+                    for (int i = 0; i < network.PuzzleStates.Count; i++)
+                    {
+                        if (network.PuzzleStates[i].name == "isBucketOnShelf")
+                            bucketFlingIndex = i;
+                        if (network.PuzzleStates[i].name == "playBucketCollisionNoise")
+                            bucketCollisionIndex = i;
+                    }
+                    networkInitialized = true;
+                    network.updatePuzzleState("isBucketOnShelf", true, "mdl_bucket");
+                }
+            }
+
+            //Check to see if audio needs to be played
+            else
+            {
+                if (network.PuzzleStates[bucketCollisionIndex].state == true)
+                {
+                    //play some audio (make sure it's a Oneshot)
+                    Debug.LogWarning("Bucket Throw Sound");
+                    network.Cmd_UpdatePuzzleLogic("playBucketCollisionNoise", false, "mdl_bucket");
+                }
+            }
         }
 
         public override void resetItem()
         {
-            //Sample puzzle state update
-            //GameObject.FindWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>().updatePuzzleState("bucketChucked", true, "mdl_bucket");
+            Debug.Log("Reset Called");
 
-            rb.useGravity = true;
-            rb.AddForce(new Vector3(950, 0, 200));
-            GetComponent<spt_interactiveMovable>().enabled = false;
+            if (networkInitialized)
+            {
+                network.updatePuzzleState("isBucketOnShelf", false, "mdl_bucket");
+
+                rb.useGravity = true;
+                rb.AddForce(new Vector3(950, 0, 200));
+                GetComponent<spt_interactiveMovable>().enabled = false;
+            }
         }
     }
 }
---
+++ Shackle/Assets/Scripts/Garage Object Scripts/spt_bucketCollision.cs
@@ -0,0 +1,52 @@
+﻿/*
+spt_bucketCollision
+
+Author(s): Lauren Cunningham
+
+Revision 1
+
+Script to detect when the bucket collides with an object after it has been thrown off the shelf by the monster.
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class spt_bucketCollision : MonoBehaviour {
+
+    bool networkInitialized = false;
+    spt_NetworkPuzzleLogic network;
+    int bucketFlingIndex = -1;
+    int bucketCollisionIndex = -1;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!networkInitialized)
+        {
+            network = GameObject.FindWithTag("Player").GetComponent<spt_NetworkPuzzleLogic>();
+            if (network != null)
+            {
+                for (int i = 0; i < network.PuzzleStates.Count; i++)
+                {
+                    if (network.PuzzleStates[i].name == "isBucketOnShelf")
+                        bucketFlingIndex = i;
+                    if (network.PuzzleStates[i].name == "playBucketCollisionNoise")
+                        bucketCollisionIndex = i;
+                }
+                networkInitialized = true;
+                network.updatePuzzleState("isBucketOnShelf", true, "mdl_bucket");
+            }
+        }
+    }
+
+    // Called when the bucket collides with an object
+    public void OnCollisionEnter(Collision c)
+    {
+        if (network.PuzzleStates[bucketFlingIndex].state == false)
+            network.Cmd_UpdatePuzzleLogic("playBucketCollisionNoise", true, "mdl_bucket");
+    }
+}
